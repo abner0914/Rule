cbdd321a12f26b759396e776e3f3d64036c4bf7e
1 0 <abner0914@gmail.com>
2019-06-21 13:03:16 +0800

    Update BTSchool.js

--- BTSchool.js
+++ BTSchool.js
@@ -10,8 +10,8 @@
 // ==/UserScript==
 
 exports.run = async function() {
-    var { data } = await axios.get('http://pt.btschool.club/index.php?action=addbonus');
-    if (data.code == 200) return '签到成功';
+    var { status, data } = await axios.get('http://pt.btschool.club/index.php?action=addbonus');
+    if (status == 200) return '签到成功';
     throw '访问失败';
 };
 
