8bb4aeb5630e21905d22bb75fc68855fb4d91d0f
1 0 <abner0914@gmail.com>
2019-06-21 11:17:38 +0800

    Create BTSchool.js

---
+++ BTSchool.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name            BTSchool
+// @namespace       https://github.com/inu1255/soulsign-chrome
+// @version         1.0.0
+// @author          GSM
+// @loginURL        http://pt.btschool.club
+// @updateURL       https://gitee.com/inu1255/soulsign-chrome/raw/master/public/demos/hdhome.js
+// @expire          14400e3
+// @domain          pt.btschool.club
+// ==/UserScript==
+
+exports.run = async function() {
+    var { data } = await axios.get('http://pt.btschool.club/index.php?action=addbonus');
+    if (/签到成功/.test(data)) return '签到成功';
+    if (/您今天已经签到过了/.test(data)) return '今日已签';
+    throw '访问失败';
+};
+
+exports.check = async function() {
+    var { data } = await axios.get('http://pt.btschool.club/index.php');
+    return /欢迎回来/.test(data);
+};
