64c99e890a0c57d7f10ac5755a245318478527c2
1 0 <abner0914@gmail.com>
2019-12-17 17:20:49 +0800

    Update pter.js

--- pter.js
+++ pter.js
@@ -3,14 +3,14 @@
 // @namespace       https://github.com/inu1255/soulsign-chrome
 // @version         1.0.0
 // @author          10
-// @loginURL        https://pter.club/
+// @loginURL        https://pterclub.com/
 // @updateURL       https://raw.githubusercontent.com/abner0914/Rule/master/pter.js
 // @expire          14400e3
-// @domain          pter.club
+// @domain          pterclub.com
 // ==/UserScript==
 
 exports.run = async function() {
-    var { status,data } = await axios.get('https://pter.club/attendance-ajax.php');
+    var { status,data } = await axios.get('https://pterclub.com/attendance-ajax.php');
     if (status == 200) return '签到成功';
     throw '访问失败';
 };
