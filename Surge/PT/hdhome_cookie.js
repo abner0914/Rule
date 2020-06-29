f7025ed8af32e07d79679c1b6bb10efb1f0b1f4e
zhao10 <abner0914@gmail.com>
2020-06-29 22:16:16 +0800

    add pt

---
+++ Surge/PT/hdhome_cookie.js
@@ -0,0 +1,15 @@
+const cookieName = 'hdhome.org'
+const cookieKey = 'cookie_hdhome'
+const cookieVal = $request.headers['Cookie']
+
+if (cookieVal) {
+  let cookie = $persistentStore.write(cookieVal, cookieKey)
+  if (cookie) {
+    let msg = `${cookieName}`
+    $notification.post(msg, 'Cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(cookieVal)
+  }
+}
+
+$done({})
