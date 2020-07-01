75ab59612da2dfe12f79fb6b62a112d49dfc45d3
zhao10 <abner0914@gmail.com>
2020-07-01 22:02:56 +0800

    update

---
+++ Surge/PT/btschool.js
@@ -0,0 +1,36 @@
+const cookieName = 'pt.btschool.club'
+const cookieKey = 'cookie_btschool'
+const cookieVal = $persistentStore.read(cookieKey)
+
+function sign() {
+  let url = {
+    url: `https://pt.btschool.club/index.php?action=addbonus`,
+    headers: {
+      Cookie: cookieVal
+    }
+  }
+  url.headers['Referer'] = 'https://pt.btschool.club/'
+  url.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+
+  $httpClient.get(url, (error, response, data) => {
+    let result = JSON.parse(data)
+    let title = `${cookieName}`
+    // 签到成功
+    if (result.code == 200) {
+      let subTitle = `签到成功`
+      let detail = `详见日志`
+      $notification.post(title, subTitle, detail)
+    }
+    // 签到失败
+    else {
+      let subTitle = `签到失败`
+      detail = `编码: ${result.code}, 说明: ${result.msg}`
+      $notification.post(title, subTitle, detail)
+    }
+    console.log(`${cookieName}, data: ${data}`)
+  })
+
+  $done({})
+}
+
+sign()
