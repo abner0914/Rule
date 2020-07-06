422225d78bb1f72680611550851e41d2688e7784
zhao10 <abner0914@gmail.com>
2020-07-07 01:33:01 +0800

    update

--- Surge/PT/ttg.js
+++ Surge/PT/ttg.js
@@ -1,16 +1,16 @@
-const cookieName = 'hdchina.org'
-const cookieKey = 'cookie_hdchina'
+const cookieName = 'totheglory.im'
+const cookieKey = 'cookie_ttg'
 const cookieVal = $persistentStore.read(cookieKey)
 
 function sign() {
   let url = {
-    url: `https://hdchina.org/plugin_sign-in.php?cmd=signin`,
+    url: `https://totheglory.im/signed.php`,
     headers: {
       Cookie: cookieVal,
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
       'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     },
-    body: 'csrf=cc850e0ae36887d009341b33163165242ce43409'
+    body: 'signed_timestamp=1594008756&signed_token=7f7754c95a4314aed4b7511ada8f011b'
   }
 
   $httpClient.post(url, (error, response, data) => {
