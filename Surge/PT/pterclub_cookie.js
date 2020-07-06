6846a9677394ea80105e6f3b687f9d19b264e43e
zhao10 <abner0914@gmail.com>
2020-07-07 01:58:37 +0800

    update

---
+++ Surge/PT/pterclub_cookie.js
@@ -0,0 +1,15 @@
+const cookieName = 'pterclub.com'
+const cookieKey = 'cookie_pterclub'
+const cookieVal = $request.headers['Cookie']
+
+if (cookieVal) {
+  let cookie = $persistentStore.write(cookieVal, cookieKey)
+  if (cookie) {
+    let msg = `${cookieName}`
+    $notification.post(msg, 'Cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(cookieVal)
+  }
+}
+
+$done({})
